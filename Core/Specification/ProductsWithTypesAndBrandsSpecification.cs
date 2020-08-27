7797bbcb4532aa9a356b2a3a84e3cf40b3e8b23a
Serhii Tsyhanivskyi <Serhii_Tsyhanivskyi@epam.com>
2020-08-27 11:19:56 +0300

    add automapping betweeb dto and entities

--- Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specification/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq.Expressions;
 using Core.Entities;
 
 namespace Core.Specification
@@ -8,6 +10,12 @@ namespace Core.Specification
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);            
         }
     }
 }
